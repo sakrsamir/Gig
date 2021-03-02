117f8d6729faf00df17606dfbccfa07e646576fc
Sakr Samir <eng.sakr88@yahoo.com>
2021-03-02 15:00:58 +0200

    CreateGigModel

---
+++ Gighub/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gighub.Models
+{
+    public class Genre
+    {
+        // we don't have more than 255 genres
+        public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+    }
+}
---
+++ Gighub/Models/Gig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gighub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        // who use it
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        // when this going to happen
+        public DateTime DateTime { get; set; }
+
+        //  where this going to happen
+        [Required]
+        [StringLength(255)]
+        public String Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+    }
+
+    
+}
