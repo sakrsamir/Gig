603428143aa72b857c3ba1dbd1181668917a3699
Sakr Samir <eng.sakr888@gmail.com>
2021-03-05 16:05:35 +0200

    AddFollowingTodomainModel

--- Gighub/Models/ApplicationDbContext.cs
+++ Gighub/Models/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace Gighub.Models
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Following> Followings { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -25,6 +26,17 @@ namespace Gighub.Models
                 .HasRequired(a => a.Gig)
                 .WithMany()
                 .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(a=>a.Followers)
+                .WithRequired(f => f.Followee)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(a => a.Followees)
+                .WithRequired(f => f.Follower)
+                .WillCascadeOnDelete(false);
+
 
             base.OnModelCreating(modelBuilder);
         }
