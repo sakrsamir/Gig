4af4b32e5910c378771af480e9a00f53879ab07a
Sakr Samir <eng.sakr888@gmail.com>
2021-03-09 21:43:01 +0200

    Implement My Upcomming Gigs

--- Gighub/Controllers/GigsController.cs
+++ Gighub/Controllers/GigsController.cs
@@ -1,6 +1,7 @@
 ﻿using Gighub.Models;
 using Gighub.ViewModels;
 using Microsoft.AspNet.Identity;
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -14,6 +15,19 @@ namespace Gighub.Controllers
         {
             _context = new ApplicationDbContext();
         }
+
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Include(g=>g.Genre)
+                .ToList();
+            return View(gigs);
+        }
+
 
         [Authorize]
         public ActionResult Attending()
@@ -67,7 +81,7 @@ namespace Gighub.Controllers
 
             _context.Gigs.Add(gig);
             _context.SaveChanges();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine");
         }
     }
 }
