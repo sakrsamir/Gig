068c1c0501cff6214963db79f71d32ab80926054
Sakr Samir <eng.sakr888@gmail.com>
2021-03-13 13:15:34 +0200

    implementDeleteWithNiceFadeOut

---
+++ Gighub/Controllers/Api/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using Gighub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace Gighub.Controllers.Api
+{
+    [Authorize]
+    public class GigsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+            gig.IsCanceled = true;
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
